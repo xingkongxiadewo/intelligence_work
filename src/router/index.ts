dd6d609693d2077d20ffd2c65236b12e099446b4
rise <tccwpl@163.com>
2022-06-09 15:11:32 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,9 @@
+import { createRouter,createWebHistory } from 'vue-router'
+import LoginPage from '../view/index/LoginPage.vue'
+const router=createRouter({
+    history:createWebHistory(),
+    routes:[
+        { path:"/login",component:LoginPage }
+    ]
+})
+export default router;
