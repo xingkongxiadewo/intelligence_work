ccfa82a9a88a0fc6032ab08918377bbb2388e10c
rise <tccwpl@163.com>
2022-06-16 17:11:50 +0800

    存档

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,13 @@ export default defineConfig({
     open:true,
     proxy:{ 
       '/api':{
-        target:'http://localhost:5294/api',
+        target:'http://192.168.3.109:8081/api',
         changeOrigin:true,
         rewrite:(path) => path.replace(/^\/api/,'')
       }
     },
+  },
+  build:{
+    target:['edge90','chrome90','firefox90','safari15']
   }
 })
