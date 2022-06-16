0efd79382dfd764d98334874b5b574319e286716
rise <tccwpl@163.com>
2022-06-16 14:26:33 +0800

    导航守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,8 @@ import RootPage from '../view/index/RootPage.vue'
 import DeskTop from '../view/index/DeskTop.vue'
 import PersonCenter from '../view/index/PersonCenter.vue'
 import { MenuModel } from '../view/admin/menu/class/MenuModel'
-// import MenuManager from '../view/admin/menu/MenuManager.vue'
-// import RoleManager from '../view/admin/role/RoleManager.vue'
-// import PersonManager from '../view/admin/person/PersonManager.vue'
+import Tool from '../global'
+import { UserInfo } from '../../src/view/index/class/UserInfo'
 import { getUserMenus } from '../http/index'
 let routes = [
     {
@@ -14,65 +13,62 @@ let routes = [
         children: [
             { name: "工作台", path: "/desktop", component: DeskTop },
             { name: "个人信息", path: "/person", component: PersonCenter },
-            // {  name:"菜单管理",path:"/menumanager",component:MenuManager  },
-            // {  name:"角色管理",path:"/rolemanager",component:RoleManager  },
-            // {  name:"用户管理",path:"/personmanager",component:PersonManager  }
         ]
     },
     { path: "/login", component: LoginPage }
 ]
-//通过json渲染路由
-// const list = [{
-//     "id": 1,
-//     "index": "rolemanager",
-//     "name": "角色管理",
-//     "filePath": "../view/admin/role/RoleManager",
-//     "parent": "rolemanager",
-//     "order": 99,
-//     "date": "2016-05-02",
-//     "isEnable": false,
-//     "children": null
-// },
-// {
-//     "id": 2,
-//     "index": "personmanager",
-//     "name": "人员管理",
-//     "filePath": "../view/admin/person/PersonManager",
-//     "parent": "rolemanager",
-//     "order": 99,
-//     "date": "2016-05-02",
-//     "isEnable": false,
-//     "children": null
-// },
-// {
-//     "id": 3,
-//     "index": "menumanager",
-//     "name": "菜单管理",
-//     "filePath": "../view/admin/menu/MenuManager",
-//     "parent": "rolemanager",
-//     "order": 99,
-//     "date": "2016-05-02",
-//     "isEnable": false,
-//     "children": null
-// }]
+
+const toolObj = new Tool()
+
+//当前存在用户信息且有效，才会读取动态路由
 if (localStorage["nickname"] != undefined) {
-    //读取webapi，获取路由列表
-    const list: MenuModel[] = await getUserMenus() as any as MenuModel[]
-    let data = []
-    if (list.length > 0) {
-        for (let i = 0; i < list.length; i++) {
-            //PS1：这里import动态路由导入，需要通过string+变量的方式导入，如果直接传入一个变量，编译器无法成功解析！ 
-            //PS2：动态导入提示警告 需要加上/* @vite-ignore */才能消除警告
-            data.push({ name: list[i].name, path: list[i].index, component: () => import(/* @vite-ignore */`${list[i].filePath}.vue`) })
+    const user: UserInfo = JSON.parse(new Tool().FormatToken(localStorage["token"]))
+    const expDate = toolObj.FormatDate(user.exp)
+    const currDate = toolObj.GetDate()
+    if (expDate >= currDate) {
+        //读取webapi，获取路由列表
+        const list: MenuModel[] = await getUserMenus() as any as MenuModel[]
+        let data = []
+        if (list.length > 0) {
+            for (let i = 0; i < list.length; i++) {
+                //PS1：这里import动态路由导入，需要通过string+变量的方式导入，如果直接传入一个变量，编译器无法成功解析！ 
+                //PS2：动态导入提示警告 需要加上/* @vite-ignore */才能消除警告
+                data.push({ name: list[i].name, path: list[i].index, component: () => import(/* @vite-ignore */`${list[i].filePath}.vue`) })
+            }
+            routes[0].children = routes[0].children?.concat(data as []) as []
         }
-        routes[0].children = routes[0].children?.concat(data as []) as []
     }
 }
-
 
 //创建路由
 const router = createRouter({
     history: createWebHistory(),
     routes: routes
+})
+
+//路由守卫
+router.beforeEach((to, form) => {
+    if (localStorage["nickname"] != undefined) {
+        const user: UserInfo = JSON.parse(new Tool().FormatToken(localStorage["token"]))
+        const expDate = toolObj.FormatDate(user.exp)
+        const currDate = toolObj.GetDate()
+        if (to.path == "/login") {
+            if (expDate >= currDate) {
+                return { path: "/desktop" }
+            } else {
+                toolObj.ClearLocalStorage()
+            }
+        } else {
+            if (expDate < currDate) {
+                toolObj.ClearLocalStorage()
+                return { path: "/login" }
+            }
+        }
+    } else {
+        //避免无限重定向，因此要做个判断
+        if (to.path !== "/login") {
+            return { path: "/login" }
+        }
+    }
 })
 export default router;
