fcc1cc903ed5627c9e3eb77b4a3398affdc2c635
rise <tccwpl@163.com>
2022-06-16 11:18:49 +0800

    动态路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -55,18 +55,20 @@ let routes = [
 //     "isEnable": false,
 //     "children": null
 // }]
-//读取webapi，获取路由列表
-const list: MenuModel[] = await getUserMenus() as any as MenuModel[]
-
-let data = []
-if (list.length > 0) {
-    for (let i = 0; i < list.length; i++) {
-        //PS1：这里import动态路由导入，需要通过string+变量的方式导入，如果直接传入一个变量，编译器无法成功解析！ 
-        //PS2：动态导入提示警告 需要加上/* @vite-ignore */才能消除警告
-        data.push({ name: list[i].name, path: list[i].index, component: () => import(/* @vite-ignore */`${list[i].filePath}.vue`) })
+if (localStorage["nickname"] != undefined) {
+    //读取webapi，获取路由列表
+    const list: MenuModel[] = await getUserMenus() as any as MenuModel[]
+    let data = []
+    if (list.length > 0) {
+        for (let i = 0; i < list.length; i++) {
+            //PS1：这里import动态路由导入，需要通过string+变量的方式导入，如果直接传入一个变量，编译器无法成功解析！ 
+            //PS2：动态导入提示警告 需要加上/* @vite-ignore */才能消除警告
+            data.push({ name: list[i].name, path: list[i].index, component: () => import(/* @vite-ignore */`${list[i].filePath}.vue`) })
+        }
+        routes[0].children = routes[0].children?.concat(data as []) as []
     }
-    routes[0].children = routes[0].children?.concat(data as []) as []
 }
+
 
 //创建路由
 const router = createRouter({
